787711ed3c11eb9a86c340d663fe09269f99b0c8
y-du <42994541+y-du@users.noreply.github.com>
2024-03-07 09:25:43 +0100

    refactor

--- handler/cloud_hdl/hub.go
+++ handler/cloud_hdl/hub.go
@@ -3,8 +3,10 @@ package cloud_hdl
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	context_hdl "github.com/SENERGY-Platform/go-service-base/context-hdl"
+	"github.com/SENERGY-Platform/mgw-device-cloud-connector/util/cloud_client"
 	"github.com/SENERGY-Platform/models/go/models"
 	"os"
 	"path"
@@ -12,7 +14,7 @@ import (
 
 const hubFile = "hub.json"
 
-type hubInfo struct {
+type hub struct {
 	ID          string            `json:"id"`
 	DeviceIDMap map[string]string `json:"device_id_map"` // localID:ID
 }
@@ -24,72 +26,82 @@ func (h *Handler) InitHub(ctx context.Context, id, name string) error {
 	if err := os.MkdirAll(h.wrkSpacePath, 0770); err != nil {
 		return err
 	}
-	hInfo, err := readHubInfo(h.wrkSpacePath)
+	hb, err := readHub(h.wrkSpacePath)
 	if err != nil && !os.IsNotExist(err) {
 		return err
 	}
 	if id != "" {
-		hInfo.ID = id
+		hb.ID = id
 	}
 	ctxWt, cf := context.WithTimeout(ctx, h.timeout)
 	defer cf()
-	if hInfo.ID != "" {
-		hub, err := h.cloudClient.GetHub(ctxWt, hInfo.ID)
+	var deviceIDs []string
+	if hb.ID != "" {
+		chb, err := h.cloudClient.GetHub(ctx, hb.ID)
 		if err != nil {
-			return err
+			var nfe *cloud_client.NotFoundError
+			if !errors.As(err, &nfe) {
+				return err
+			}
+			hb.ID, err = h.cloudClient.CreateHub(ctxWt, models.Hub{Name: name})
+			if err != nil {
+				return err
+			}
 		}
-		hInfo.DeviceIDMap, err = h.getDeviceIDMap(ctx, hInfo.DeviceIDMap, hub.DeviceIds)
-		if err != nil {
-			return err
-		}
+		deviceIDs = chb.DeviceIds
 	} else {
-		id, err = h.cloudClient.CreateHub(ctxWt, models.Hub{Name: name})
+		hb.ID, err = h.cloudClient.CreateHub(ctxWt, models.Hub{Name: name})
 		if err != nil {
 			return err
 		}
-		hInfo.ID = id
 	}
-	h.hubInfo = hInfo
-	return writeHubInfo(h.wrkSpacePath, h.hubInfo)
+	hb.DeviceIDMap, err = h.getDeviceIDMap(ctxWt, hb.DeviceIDMap, deviceIDs)
+	if err != nil {
+		return err
+	}
+	h.hub = hb
+	return writeHub(h.wrkSpacePath, h.hub)
 }
 
 func (h *Handler) getDeviceIDMap(ctx context.Context, oldMap map[string]string, deviceIDs []string) (map[string]string, error) {
-	ch := context_hdl.New()
-	defer ch.CancelAll()
 	deviceIDMap := make(map[string]string)
-	rDeviceIDMap := make(map[string]string)
-	for ldID, dID := range oldMap {
-		rDeviceIDMap[dID] = ldID
-	}
-	for _, dID := range deviceIDs {
-		ldID, ok := rDeviceIDMap[dID]
-		if !ok {
-			device, err := h.cloudClient.GetDevice(ch.Add(context.WithTimeout(ctx, h.timeout)), dID)
-			if err != nil {
-				return nil, err
-			}
-			ldID = device.LocalId
+	if len(deviceIDs) > 0 {
+		ch := context_hdl.New()
+		defer ch.CancelAll()
+		rDeviceIDMap := make(map[string]string)
+		for ldID, dID := range oldMap {
+			rDeviceIDMap[dID] = ldID
 		}
-		deviceIDMap[ldID] = dID
+		for _, dID := range deviceIDs {
+			ldID, ok := rDeviceIDMap[dID]
+			if !ok {
+				device, err := h.cloudClient.GetDevice(ch.Add(context.WithTimeout(ctx, h.timeout)), dID)
+				if err != nil {
+					return nil, err
+				}
+				ldID = device.LocalId
+			}
+			deviceIDMap[ldID] = dID
+		}
 	}
 	return deviceIDMap, nil
 }
 
-func readHubInfo(p string) (hubInfo, error) {
+func readHub(p string) (hub, error) {
 	f, err := os.Open(path.Join(p, hubFile))
 	if err != nil {
-		return hubInfo{}, err
+		return hub{}, err
 	}
 	defer f.Close()
 	d := json.NewDecoder(f)
-	var hi hubInfo
+	var hi hub
 	if err = d.Decode(&hi); err != nil {
-		return hubInfo{}, err
+		return hub{}, err
 	}
 	return hi, nil
 }
 
-func writeHubInfo(p string, hi hubInfo) error {
+func writeHub(p string, hi hub) error {
 	f, err := os.OpenFile(path.Join(p, hubFile), os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
 	if err != nil {
 		return err
