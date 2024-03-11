f9f504b3f4f047962767c82be42ea679dbd31275
y-du <42994541+y-du@users.noreply.github.com>
2024-03-11 14:46:13 +0100

    init

---
+++ handler/device_hdl/handler_test.go
@@ -0,0 +1,364 @@
+package device_hdl
+
+import (
+	"context"
+	"github.com/SENERGY-Platform/mgw-device-cloud-connector/model"
+	"github.com/SENERGY-Platform/mgw-device-cloud-connector/util/dm_client"
+	"reflect"
+	"testing"
+)
+
+func Test_newDevice(t *testing.T) {
+	a := device{
+		Device: model.Device{
+			ID:    "test",
+			Name:  "Test Device",
+			State: "online",
+			Type:  "test-type",
+			Attributes: []model.Attribute{
+				{Key: "key", Value: "val"},
+			},
+		},
+		Hash: "6733f4db65b8824b057029b33e9142486f505217",
+	}
+	dmDevice := dm_client.Device{
+		Name:     "Test Device",
+		State:    "online",
+		Type:     "test-type",
+		ModuleID: "mid",
+		Attributes: []dm_client.Attribute{
+			{Key: "key", Value: "val"},
+		},
+	}
+	b := newDevice("test", dmDevice)
+	if !reflect.DeepEqual(a, b) {
+		t.Errorf("%+v != %+v", a, b)
+	}
+	dmDevice.Name = "Test"
+	c := newDevice("test", dmDevice)
+	if a.Hash == c.Hash {
+		t.Error("hash did not change")
+	}
+}
+
+func Test_diffDevices(t *testing.T) {
+	h := New(nil, 0, 0)
+	hReset := func() {
+		h.devices = nil
+	}
+	devices := map[string]device{
+		"a": newDevice("a", dm_client.Device{
+			Name:  "Test Device A",
+			State: "online",
+		}),
+		"b": newDevice("b", dm_client.Device{
+			Name:  "Test Device B",
+			State: "offline",
+		}),
+		"c": newDevice("c", dm_client.Device{
+			Name:  "Test Device C",
+			State: "",
+		}),
+	}
+	t.Run("new devices", func(t *testing.T) {
+		t.Cleanup(hReset)
+		h.devices = make(map[string]device)
+		changedIDs, missingIDs, deviceMap, deviceStates := h.diffDevices(devices)
+		if len(changedIDs) > 0 {
+			t.Error("illegal number of ids")
+		}
+		if len(missingIDs) > 0 {
+			t.Error("illegal number of ids")
+		}
+		for id, d := range devices {
+			if _, ok := deviceMap[id]; !ok {
+				t.Errorf("id '%s' not in map", id)
+			}
+			if d.State != "" {
+				state, ok := deviceStates[id]
+				if !ok {
+					t.Errorf("id '%s' not in state map", id)
+				}
+				if state[0] != "" {
+					t.Errorf("prev state mismatch")
+				}
+				if state[1] != d.State {
+					t.Errorf("current state mismatch")
+				}
+			} else {
+				if _, ok := deviceStates[id]; ok {
+					t.Errorf("id '%s' in state map", id)
+				}
+			}
+		}
+	})
+	t.Run("unchanged devices", func(t *testing.T) {
+		t.Cleanup(hReset)
+		h.devices = devices
+		changedIDs, missingIDs, deviceMap, deviceStates := h.diffDevices(devices)
+		if len(changedIDs) > 0 {
+			t.Error("illegal number of ids")
+		}
+		if len(missingIDs) > 0 {
+			t.Error("illegal number of ids")
+		}
+		if len(deviceStates) > 0 {
+			t.Error("illegal number of states")
+		}
+		for id, _ := range devices {
+			if _, ok := deviceMap[id]; !ok {
+				t.Errorf("id '%s' not in map", id)
+			}
+		}
+	})
+	t.Run("changed devices", func(t *testing.T) {
+		t.Cleanup(hReset)
+		h.devices = devices
+		devices2 := map[string]device{
+			"a": newDevice("a", dm_client.Device{
+				Name:  "Test Device",
+				State: "online",
+			}),
+			"b": newDevice("b", dm_client.Device{
+				Name:  "Test Device B",
+				State: "online",
+			}),
+			"c": newDevice("c", dm_client.Device{
+				Name:  "Test Device C",
+				State: "offline",
+			}),
+		}
+		changedIDs, missingIDs, deviceMap, deviceStates := h.diffDevices(devices2)
+		if !inSlice("a", changedIDs) {
+			t.Error("missing id")
+		}
+		if len(missingIDs) > 0 {
+			t.Error("illegal number of ids")
+		}
+		for id := range devices2 {
+			if _, ok := deviceMap[id]; !ok {
+				t.Errorf("id '%s' not in map", id)
+			}
+		}
+		state, ok := deviceStates["b"]
+		if !ok {
+			t.Error("id not in state map")
+		}
+		if state[0] != "offline" {
+			t.Errorf("prev state mismatch")
+		}
+		if state[1] != "online" {
+			t.Errorf("current state mismatch")
+		}
+		state, ok = deviceStates["c"]
+		if !ok {
+			t.Error("id not in state map")
+		}
+		if state[0] != "" {
+			t.Errorf("prev state mismatch")
+		}
+		if state[1] != "offline" {
+			t.Errorf("current state mismatch")
+		}
+	})
+	t.Run("missing devices", func(t *testing.T) {
+		t.Cleanup(hReset)
+		h.devices = devices
+		devices2 := map[string]device{
+			"a": newDevice("a", dm_client.Device{
+				Name:  "Test Device A",
+				State: "online",
+			}),
+			"b": newDevice("b", dm_client.Device{
+				Name:  "Test Device B",
+				State: "offline",
+			}),
+		}
+		changedIDs, missingIDs, deviceMap, deviceStates := h.diffDevices(devices2)
+		if len(changedIDs) > 0 {
+			t.Error("illegal number of ids")
+		}
+		if !inSlice("c", missingIDs) {
+			t.Error("missing id")
+		}
+		if len(deviceStates) > 0 {
+			t.Error("illegal number of states")
+		}
+		for id, _ := range devices2 {
+			if _, ok := deviceMap[id]; !ok {
+				t.Errorf("id '%s' not in map", id)
+			}
+		}
+	})
+}
+
+func Test_refreshDevices(t *testing.T) {
+	mockDMC := &dm_client.Mock{}
+	h := New(mockDMC, 0, 0)
+	hReset := func() {
+		h.devices = nil
+	}
+	t.Run("new, changed and missing device", func(t *testing.T) {
+		t.Cleanup(mockDMC.Reset)
+		t.Cleanup(hReset)
+		h.devices = map[string]device{
+			"a": newDevice("a", dm_client.Device{
+				Name:  "Test Device",
+				State: "offline",
+			}),
+			"b": newDevice("b", dm_client.Device{
+				Name:  "Test Device B",
+				State: "offline",
+			}),
+		}
+		mockDMC.Devices = map[string]dm_client.Device{
+			"a": {
+				Name:  "Test Device A",
+				State: "online",
+			},
+			"c": {
+				Name:  "Test Device C",
+				State: "",
+			},
+		}
+		syncCall := 0
+		h.SetSyncCallback(func(_ context.Context, devices map[string]model.Device, changedIDs, missingIDs []string) (failed []string, err error) {
+			syncCall += 1
+			for id := range mockDMC.Devices {
+				if _, ok := devices[id]; !ok {
+					t.Errorf("id '%s' not in map", id)
+				}
+			}
+			if !inSlice("a", changedIDs) {
+				t.Error("missing id")
+			}
+			if !inSlice("b", missingIDs) {
+				t.Error("missing id")
+			}
+			return nil, err
+		})
+		stateCall := 0
+		h.SetStateCallback(func(_ context.Context, deviceStates map[string]string) (failed []string, err error) {
+			stateCall += 1
+			state, ok := deviceStates["a"]
+			if !ok {
+				t.Error("id not in map")
+			}
+			if state != "online" {
+				t.Error("state mismatch")
+			}
+			return nil, err
+		})
+		err := h.refreshDevices(context.Background())
+		if err != nil {
+			t.Error(err)
+		}
+		if syncCall != 1 {
+			t.Error("illegal number of sync calls")
+		}
+		if stateCall != 1 {
+			t.Error("illegal number of state change calls")
+		}
+		if len(h.devices) > 2 {
+			t.Error("illegal number of devices")
+		}
+		for id := range mockDMC.Devices {
+			if _, ok := h.devices[id]; !ok {
+				t.Errorf("id '%s' not in map", id)
+			}
+		}
+	})
+	t.Run("failed devices", func(t *testing.T) {
+		t.Cleanup(mockDMC.Reset)
+		t.Cleanup(hReset)
+		h.devices = map[string]device{
+			"a": newDevice("a", dm_client.Device{
+				Name:  "Test Device",
+				State: "offline",
+			}),
+			"b": newDevice("b", dm_client.Device{
+				Name:  "Test Device B",
+				State: "offline",
+			}),
+		}
+		mockDMC.Devices = map[string]dm_client.Device{
+			"a": {
+				Name:  "Test Device A",
+				State: "online",
+			},
+			"c": {
+				Name:  "Test Device C",
+				State: "",
+			},
+		}
+		syncCall := 0
+		h.SetSyncCallback(func(_ context.Context, devices map[string]model.Device, changedIDs, missingIDs []string) (failed []string, err error) {
+			syncCall += 1
+			for id := range mockDMC.Devices {
+				if _, ok := devices[id]; !ok {
+					t.Errorf("id '%s' not in map", id)
+				}
+			}
+			if !inSlice("a", changedIDs) {
+				t.Error("missing id")
+			}
+			if syncCall == 1 && !inSlice("b", missingIDs) {
+				t.Error("missing id")
+			}
+			if syncCall == 2 && len(missingIDs) > 0 {
+				t.Error("illegal number of ids")
+			}
+			return []string{"a", "c"}, nil
+		})
+		stateCall := 0
+		h.SetStateCallback(func(_ context.Context, deviceStates map[string]string) (failed []string, err error) {
+			stateCall += 1
+			state, ok := deviceStates["a"]
+			if !ok {
+				t.Error("id not in map")
+			}
+			if state != "online" {
+				t.Error("state mismatch")
+			}
+			return []string{"a"}, nil
+		})
+		err := h.refreshDevices(context.Background())
+		if err != nil {
+			t.Error(err)
+		}
+		err = h.refreshDevices(context.Background())
+		if err != nil {
+			t.Error(err)
+		}
+		if syncCall != 2 {
+			t.Error("illegal number of sync calls")
+		}
+		if stateCall != 2 {
+			t.Error("illegal number of state change calls")
+		}
+		if len(h.devices) != 1 {
+			t.Error("illegal number of devices")
+		}
+		if _, ok := h.devices["a"]; !ok {
+			t.Error("id not in map")
+		}
+	})
+	t.Run("error case", func(t *testing.T) {
+		t.Cleanup(mockDMC.Reset)
+		t.Cleanup(hReset)
+		mockDMC.SetInternalErr()
+		err := h.refreshDevices(context.Background())
+		if err == nil {
+			t.Error(err)
+		}
+	})
+}
+
+func inSlice(s string, sl []string) bool {
+	for _, s2 := range sl {
+		if s2 == s {
+			return true
+		}
+	}
+	return false
+}
