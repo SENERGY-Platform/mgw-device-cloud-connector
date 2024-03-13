7efcda2c6488df6d17df78a1264fabf5293c666f
y-du <42994541+y-du@users.noreply.github.com>
2024-03-13 15:28:41 +0100

    fix topic parts

--- handler/message_hdl/topic.go
+++ handler/message_hdl/topic.go
@@ -8,9 +8,10 @@ const (
 	slash             byte = '/'
 )
 
-func matchTopic(topic, str string, tParts []string) bool {
+func matchTopic(topic, str string, tParts ...*string) bool {
 	shift := 0
 	strLen := len(str)
+	pCount := 0
 	var i int
 	for i = 0; i < len(topic); i++ {
 		if i+shift >= strLen {
@@ -22,11 +23,12 @@ func matchTopic(topic, str string, tParts []string) bool {
 			if pos < 0 {
 				pos = len(str[i+shift:])
 			}
-			tPart := str[i+shift : i+shift+pos]
-			shift += len(tPart) - 1
-			tParts = append(tParts, tPart)
+			tp := str[i+shift : i+shift+pos]
+			*tParts[pCount] = tp
+			pCount++
+			shift += len(tp) - 1
 		case multiLvlWildcard:
-			tParts = append(tParts, str[i+shift:])
+			*tParts[pCount] = str[i+shift:]
 			return true
 		case str[i+shift]:
 		default:
