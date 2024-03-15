8ac4ae0895b7eac604e81333cc99c3484a8dc8ee
y-du <42994541+y-du@users.noreply.github.com>
2024-03-15 11:32:49 +0100

    init

---
+++ util/topic.go
@@ -0,0 +1,23 @@
+package util
+
+const (
+	CloudDeviceCmdSub          = "command/+/+"       // command/{local_device_id}/+
+	CloudProcessesCmdSub       = "processes/+/cmd/#" // processes/{hub_id}/cmd/#
+	CloudDeviceEventPub        = "event"             // event/{local_device_id}/{service_id}
+	CloudDeviceCmdResponsePub  = "response"          // response/{local_device_id}/{service_id}
+	CloudProcessesStatePub     = "processes"         // processes/{hub_id}/state/{sub_topic}
+	CloudDeviceConnectorErrPub = "error"
+	CloudDeviceErrPub          = "error/device"  //  error/device/{local_device_id}
+	CloudDeviceCmdErrPub       = "error/command" // error/command/{correlation_id}
+)
+
+const (
+	LocalDeviceEventSub        = "event/#"
+	LocalDeviceCmdResponseSub  = "response/#"
+	LocalProcessesStateSub     = "processes/state/#"
+	LocalDeviceConnectorErrSub = "error/client"
+	LocalDeviceErrSub          = "error/device/+"
+	LocalDeviceCmdErrSub       = "error/command/+"
+	LocalDeviceCmdPub          = "command"
+	LocalProcessesCmdPub       = "processes/cmd"
+)
