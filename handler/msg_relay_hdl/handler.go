87345d9073296d308569e26f583cbbb66cb30fe5
y-du <42994541+y-du@users.noreply.github.com>
2024-03-20 08:21:12 +0100

    refactor

--- handler/msg_relay_hdl/handler.go
+++ handler/msg_relay_hdl/handler.go
@@ -2,33 +2,29 @@ package msg_relay_hdl
 
 import (
 	"errors"
+	"github.com/SENERGY-Platform/mgw-device-cloud-connector/handler"
 	"github.com/SENERGY-Platform/mgw-device-cloud-connector/util"
 	"sync"
 )
 
-type Message interface {
-	Topic() string
-	Payload() []byte
-}
-
 type Handler struct {
-	messages   chan Message
-	handleFunc func(m Message) (topic string, data []byte, err error)
+	messages   chan handler.Message
+	handleFunc func(m handler.Message) (topic string, data []byte, err error)
 	sendFunc   func(topic string, data []byte) error
 	started    bool
 	closed     bool
 	mu         sync.RWMutex
 }
 
-func New(buffer int, handleFunc func(m Message) (topic string, data []byte, err error), sendFunc func(topic string, data []byte) error) *Handler {
+func New(buffer int, handleFunc func(m handler.Message) (topic string, data []byte, err error), sendFunc func(topic string, data []byte) error) *Handler {
 	return &Handler{
-		messages:   make(chan Message, buffer),
+		messages:   make(chan handler.Message, buffer),
 		handleFunc: handleFunc,
 		sendFunc:   sendFunc,
 	}
 }
 
-func (h *Handler) Put(m Message) error {
+func (h *Handler) Put(m handler.Message) error {
 	select {
 	case h.messages <- m:
 	default:
