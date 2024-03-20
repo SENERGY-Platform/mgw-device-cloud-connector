40acac0696b011f83e433c0fed9de67768d939cb
y-du <42994541+y-du@users.noreply.github.com>
2024-03-20 15:23:02 +0100

    init

---
+++ handler/message_hdl/processes.go
@@ -0,0 +1,22 @@
+package message_hdl
+
+import (
+	"github.com/SENERGY-Platform/mgw-device-cloud-connector/handler"
+	"github.com/SENERGY-Platform/mgw-device-cloud-connector/util/topic"
+)
+
+func HandleDownstreamProcessesCmd(m handler.Message) (string, []byte, error) {
+	var hID, subTopic string
+	if !parseTopic(topic.CloudProcessesCmdSub, m.Topic(), &hID, &subTopic) {
+		return "", nil, newParseErr(m.Topic())
+	}
+	return topic.LocalProcessesCmdPub + "/" + subTopic, m.Payload(), nil
+}
+
+func HandleUpstreamProcessesState(m handler.Message) (string, []byte, error) {
+	var subTopic string
+	if !parseTopic(topic.LocalProcessesStateSub, m.Topic(), &subTopic) {
+		return "", nil, newParseErr(m.Topic())
+	}
+	return topic.CloudProcessesStatePub + "/" + subTopic, m.Payload(), nil
+}
