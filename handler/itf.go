4a287db8fabe7ad5302b4161ad7d17309f72eb43
y-du <42994541+y-du@users.noreply.github.com>
2024-03-21 10:22:10 +0100

    add interfaces

--- handler/itf.go
+++ handler/itf.go
@@ -1,8 +1,22 @@
 package handler
 
+import "github.com/SENERGY-Platform/mgw-device-cloud-connector/model"
+
 type MessageHandler func(m Message) (topic string, data []byte, err error)
 
 type Message interface {
 	Topic() string
 	Payload() []byte
+}
+
+type MessageRelayHandler interface {
+	Put(m Message) error
+}
+
+type CloudDeviceHandler interface {
+	GetHubID() string
+}
+
+type LocalDeviceHandler interface {
+	GetDevices() map[string]model.Device
 }
