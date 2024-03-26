6f10aa557f57db6a37c6dcc4184296c60b80e7de
y-du <42994541+y-du@users.noreply.github.com>
2024-03-26 14:22:16 +0100

    remove

--- model/util.go
+++
@@ -1,7 +0,0 @@
-package model
-
-const (
-	RelayMsgErrString    = "relaying message failed: topic=%s err=%s"
-	SubscribeErrString   = "subscribing to '%s' failed: %s"
-	UnsubscribeErrString = "unsubscribing from '%s' failed: %s"
-)
