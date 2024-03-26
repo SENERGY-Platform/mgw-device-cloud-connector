ee5461ea05569fbad4cc2f334036864d28ce8511
y-du <42994541+y-du@users.noreply.github.com>
2024-03-26 14:24:12 +0100

    refactor and improve error messages

--- handler/local_mqtt_hdl/handler.go
+++ handler/local_mqtt_hdl/handler.go
@@ -2,9 +2,14 @@ package local_mqtt_hdl
 
 import (
 	"github.com/SENERGY-Platform/mgw-device-cloud-connector/handler"
-	"github.com/SENERGY-Platform/mgw-device-cloud-connector/model"
 	"github.com/SENERGY-Platform/mgw-device-cloud-connector/util"
 	"github.com/SENERGY-Platform/mgw-device-cloud-connector/util/topic"
+)
+
+const (
+	RelayMsgErrString  = "relaying message failed: topic=%s err=%s"
+	SubscribeString    = "subscribing to local topic '%s'"
+	SubscribeErrString = "subscribing to local topic '%s' failed: %s"
 )
 
 type Handler struct {
@@ -40,50 +45,50 @@ func (h *Handler) SetMessageRelayHdl(deviceEventMsgRelayHdl, deviceCmdRespMsgRel
 func (h *Handler) HandleSubscriptions() {
 	err := h.client.Subscribe(topic.LocalDeviceEventSub, h.qos, func(m handler.Message) {
 		if err := h.deviceEventMsgRelayHdl.Put(m); err != nil {
-			util.Logger.Errorf(model.RelayMsgErrString, m.Topic(), err)
+			util.Logger.Errorf(RelayMsgErrString, m.Topic(), err)
 		}
 	})
 	if err != nil {
-		util.Logger.Errorf(model.SubscribeErrString, topic.LocalDeviceEventSub, err)
+		util.Logger.Errorf(SubscribeErrString, topic.LocalDeviceEventSub, err)
 	}
 	err = h.client.Subscribe(topic.LocalDeviceCmdResponseSub, h.qos, func(m handler.Message) {
 		if err := h.deviceCmdRespMsgRelayHdl.Put(m); err != nil {
-			util.Logger.Errorf(model.RelayMsgErrString, m.Topic(), err)
+			util.Logger.Errorf(RelayMsgErrString, m.Topic(), err)
 		}
 	})
 	if err != nil {
-		util.Logger.Errorf(model.SubscribeErrString, topic.LocalDeviceCmdResponseSub, err)
+		util.Logger.Errorf(SubscribeErrString, topic.LocalDeviceCmdResponseSub, err)
 	}
 	err = h.client.Subscribe(topic.LocalProcessesStateSub, h.qos, func(m handler.Message) {
 		if err := h.processesStateMsgRelayHdl.Put(m); err != nil {
-			util.Logger.Errorf(model.RelayMsgErrString, m.Topic(), err)
+			util.Logger.Errorf(RelayMsgErrString, m.Topic(), err)
 		}
 	})
 	if err != nil {
-		util.Logger.Errorf(model.SubscribeErrString, topic.LocalProcessesStateSub, err)
+		util.Logger.Errorf(SubscribeErrString, topic.LocalProcessesStateSub, err)
 	}
 	err = h.client.Subscribe(topic.LocalDeviceConnectorErrSub, h.qos, func(m handler.Message) {
 		if err := h.deviceConnectorErrMsgRelayHdl.Put(m); err != nil {
-			util.Logger.Errorf(model.RelayMsgErrString, m.Topic(), err)
+			util.Logger.Errorf(RelayMsgErrString, m.Topic(), err)
 		}
 	})
 	if err != nil {
-		util.Logger.Errorf(model.SubscribeErrString, topic.LocalDeviceConnectorErrSub, err)
+		util.Logger.Errorf(SubscribeErrString, topic.LocalDeviceConnectorErrSub, err)
 	}
 	err = h.client.Subscribe(topic.LocalDeviceErrSub, h.qos, func(m handler.Message) {
 		if err := h.deviceErrMsgRelayHdl.Put(m); err != nil {
-			util.Logger.Errorf(model.RelayMsgErrString, m.Topic(), err)
+			util.Logger.Errorf(RelayMsgErrString, m.Topic(), err)
 		}
 	})
 	if err != nil {
-		util.Logger.Errorf(model.SubscribeErrString, topic.LocalDeviceErrSub, err)
+		util.Logger.Errorf(SubscribeErrString, topic.LocalDeviceErrSub, err)
 	}
 	err = h.client.Subscribe(topic.LocalDeviceCmdErrSub, h.qos, func(m handler.Message) {
 		if err := h.deviceCmdErrMsgRelayHdl.Put(m); err != nil {
-			util.Logger.Errorf(model.RelayMsgErrString, m.Topic(), err)
+			util.Logger.Errorf(RelayMsgErrString, m.Topic(), err)
 		}
 	})
 	if err != nil {
-		util.Logger.Errorf(model.SubscribeErrString, topic.LocalDeviceCmdErrSub, err)
+		util.Logger.Errorf(SubscribeErrString, topic.LocalDeviceCmdErrSub, err)
 	}
 }
