e73090792eb0b9da8965b25db70225b742563e59
y-du <42994541+y-du@users.noreply.github.com>
2024-04-10 15:25:20 +0200

    init

---
+++ model/const.go
@@ -0,0 +1,10 @@
+package model
+
+const (
+	RelayMsgErrString    = "%s relay message to topic (%s): %s"
+	SubscribeString      = "%s subscribe topic (%s)"
+	SubscribeErrString   = "%s subscribe topic (%s): %s"
+	UnsubscribeString    = "%s unsubscribe topic (%s)"
+	UnsubscribeErrString = "%s unsubscribe topic (%s): %s"
+	ResubscribeString    = "%s resubscribe topic (%s)"
+)
