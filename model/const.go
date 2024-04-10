1b5ffeb7cbf2d750716d16544bda5c8f040828b4
y-du <42994541+y-du@users.noreply.github.com>
2024-04-10 16:01:21 +0200

    add log message strings

--- model/const.go
+++ model/const.go
@@ -3,8 +3,11 @@ package model
 const (
 	RelayMsgErrString    = "%s relay message to topic (%s): %s"
 	SubscribeString      = "%s subscribe topic (%s)"
+	SubscribedString     = "%s subscribed topic (%s)"
 	SubscribeErrString   = "%s subscribe topic (%s): %s"
 	UnsubscribeString    = "%s unsubscribe topic (%s)"
+	UnsubscribedString   = "%s unsubscribed topic (%s)"
 	UnsubscribeErrString = "%s unsubscribe topic (%s): %s"
 	ResubscribeString    = "%s resubscribe topic (%s)"
+	ResubscribedString   = "%s resubscribed topic (%s)"
 )
