c9cbf4d2ce695d11848dfd3dbcced07b03f92f1d
y-du <42994541+y-du@users.noreply.github.com>
2024-06-13 11:19:23 +0200

    remove set token method and add get user id method

--- util/auth_client/client.go
+++ util/auth_client/client.go
@@ -2,6 +2,8 @@ package auth_client
 
 import (
 	"context"
+	"encoding/base64"
+	"encoding/json"
 	"errors"
 	"fmt"
 	base_client "github.com/SENERGY-Platform/go-base-http-client"
@@ -23,6 +25,10 @@ type openidToken struct {
 	requestTime      time.Time
 }
 
+type jwtPayload struct {
+	Sub string `json:"sub"`
+}
+
 type Client struct {
 	baseClient *base_client.Client
 	baseUrl    string
@@ -30,7 +36,7 @@ type Client struct {
 	pw         string
 	clientID   string
 	token      *openidToken
-	mu         sync.RWMutex
+	mu         sync.Mutex
 }
 
 func New(httpClient base_client.HTTPClient, baseUrl, user, password, clientID string) *Client {
@@ -43,19 +49,39 @@ func New(httpClient base_client.HTTPClient, baseUrl, user, password, clientID st
 	}
 }
 
-func (c *Client) SetHeader(req *http.Request) bool {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
-	if c.token != nil && time.Since(c.token.requestTime).Seconds() < c.token.ExpiresIn {
-		req.Header.Set("Authorization", fmt.Sprintf("%s %s", c.token.TokenType, c.token.AccessToken))
-		return true
-	}
-	return false
-}
-
-func (c *Client) SetToken(ctx context.Context) (err error) {
+func (c *Client) SetHeader(ctx context.Context, req *http.Request) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
+	if err := c.handleToken(ctx); err != nil {
+		return err
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("%s %s", c.token.TokenType, c.token.AccessToken))
+	return nil
+}
+
+func (c *Client) GetUserID(ctx context.Context) (string, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if err := c.handleToken(ctx); err != nil {
+		return "", err
+	}
+	parts := strings.Split(c.token.AccessToken, ".")
+	if len(parts) != 3 {
+		return "", errors.New("malformed access token")
+	}
+	bytes, err := base64.RawURLEncoding.DecodeString(parts[1])
+	if err != nil {
+		return "", err
+	}
+	var payload jwtPayload
+	err = json.Unmarshal(bytes, &payload)
+	if err != nil {
+		return "", err
+	}
+	return payload.Sub, nil
+}
+
+func (c *Client) handleToken(ctx context.Context) (err error) {
 	if c.token != nil {
 		if time.Since(c.token.requestTime).Seconds() >= c.token.ExpiresIn {
 			if time.Since(c.token.requestTime).Seconds() >= c.token.RefreshExpiresIn {
