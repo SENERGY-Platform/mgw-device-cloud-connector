6e07b59b6cf75b739b12ae1134214f1e48ed6131
y-du <42994541+y-du@users.noreply.github.com>
2024-07-09 08:45:04 +0200

    add tests

---
+++ handler/msg_relay_hdl/handler_test.go
@@ -0,0 +1,115 @@
+package msg_relay_hdl
+
+import (
+	"bytes"
+	"errors"
+	sb_util "github.com/SENERGY-Platform/go-service-base/util"
+	"github.com/SENERGY-Platform/mgw-device-cloud-connector/handler"
+	"github.com/SENERGY-Platform/mgw-device-cloud-connector/model"
+	"github.com/SENERGY-Platform/mgw-device-cloud-connector/util"
+	"reflect"
+	"testing"
+	"time"
+)
+
+type mockMessage struct {
+	topic     string
+	payload   []byte
+	timestamp time.Time
+}
+
+func (m *mockMessage) Topic() string {
+	return m.topic
+}
+
+func (m *mockMessage) Payload() []byte {
+	return m.payload
+}
+
+func (m *mockMessage) Timestamp() time.Time {
+	return m.timestamp
+}
+
+func TestHandler(t *testing.T) {
+	util.InitLogger(sb_util.LoggerConfig{Terminal: true, Level: 4})
+	msg := &mockMessage{
+		topic:     "test",
+		payload:   []byte("test"),
+		timestamp: time.Now(),
+	}
+	testMsgHdl := func(m handler.Message) (topic string, data []byte, err error) {
+		if !reflect.DeepEqual(m, msg) {
+			t.Error("expected", msg, "got", m)
+		}
+		return "topic", []byte("payload"), nil
+	}
+	testSendFunc := func(topic string, data []byte) error {
+		if topic != "topic" {
+			t.Error("expected topic got", topic)
+		}
+		if !bytes.Equal(data, []byte("payload")) {
+			t.Error("expected payload got", string(data))
+		}
+		return nil
+	}
+	h := New(1, testMsgHdl, testSendFunc)
+	err := h.Put(msg)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(h.messages) != 1 {
+		t.Error("message not in channel")
+	}
+	h.Start()
+	time.Sleep(1 * time.Second)
+	if len(h.messages) > 0 {
+		t.Error("message not consumed")
+	}
+	h.Stop()
+	t.Run("message handler error", func(t *testing.T) {
+		testMsgHdl = func(m handler.Message) (topic string, data []byte, err error) {
+			return "", nil, errors.New("test error")
+		}
+		testSendFunc = func(topic string, data []byte) error {
+			t.Error("illegal call")
+			return nil
+		}
+		h = New(1, testMsgHdl, testSendFunc)
+		err = h.Put(msg)
+		if err != nil {
+			t.Error(err)
+		}
+		if len(h.messages) != 1 {
+			t.Error("message not in channel")
+		}
+		h.Start()
+		time.Sleep(1 * time.Second)
+		if len(h.messages) > 0 {
+			t.Error("message not consumed")
+		}
+		h.Stop()
+	})
+	t.Run("no message", func(t *testing.T) {
+		testMsgHdl = func(m handler.Message) (topic string, data []byte, err error) {
+			return "", nil, model.NoMsgErr
+		}
+		testSendFunc = func(topic string, data []byte) error {
+			t.Error("illegal call")
+			return nil
+		}
+		h = New(1, testMsgHdl, testSendFunc)
+		err = h.Put(msg)
+		if err != nil {
+			t.Error(err)
+		}
+		if len(h.messages) != 1 {
+			t.Error("message not in channel")
+		}
+		h.Start()
+		time.Sleep(1 * time.Second)
+		if len(h.messages) > 0 {
+			t.Error("message not consumed")
+		}
+		h.Stop()
+	})
+}
