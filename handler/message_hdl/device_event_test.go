de0ebe9af9c59fbbe77e3dbe77af69589a85a219
y-du <42994541+y-du@users.noreply.github.com>
2024-07-10 09:42:32 +0200

    add test

---
+++ handler/message_hdl/device_event_test.go
@@ -0,0 +1,61 @@
+package message_hdl
+
+import (
+	"encoding/json"
+	sb_util "github.com/SENERGY-Platform/go-service-base/util"
+	"github.com/SENERGY-Platform/mgw-device-cloud-connector/model"
+	"github.com/SENERGY-Platform/mgw-device-cloud-connector/util"
+	"github.com/SENERGY-Platform/mgw-device-cloud-connector/util/topic"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestHandleUpstreamDeviceEvent(t *testing.T) {
+	t.Cleanup(clearVars)
+	util.InitLogger(sb_util.LoggerConfig{Terminal: true, Level: 4})
+	topic.InitTopicHandler("usrID", "netID")
+	DeviceEventMaxAge = time.Second * 5
+	LocalDeviceIDPrefix = "123"
+	a := CloudDeviceEventMsg{Data: "test"}
+	rt, rb, err := HandleUpstreamDeviceEvent(&mockMessage{
+		topic:     "event/a/b",
+		payload:   []byte("test"),
+		timestamp: time.Now(),
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	if rt != topic.Handler.CloudDeviceEventPub(LocalDeviceIDPrefix+"a", "b") {
+		t.Error("expected", topic.Handler.CloudDeviceEventPub("a", "b"), "got", rt)
+	}
+	var b CloudDeviceEventMsg
+	err = json.Unmarshal(rb, &b)
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(a, b) {
+		t.Error("expected", a, "got", b)
+	}
+	t.Run("no message", func(t *testing.T) {
+		_, _, err = HandleUpstreamDeviceEvent(&mockMessage{
+			topic:   "event/a/b",
+			payload: []byte("test"),
+		})
+		if err != model.NoMsgErr {
+			t.Error("expected no message error")
+		}
+	})
+	t.Run("error", func(t *testing.T) {
+		_, _, err = HandleUpstreamDeviceEvent(&mockMessage{
+			topic:     "test",
+			timestamp: time.Now(),
+		})
+		if err == nil {
+			t.Error("expected error")
+		}
+		if err == model.NoMsgErr {
+			t.Error("wrong error type")
+		}
+	})
+}
