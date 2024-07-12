11271a3451b554bd80e34caa236314ba8de06173
y-du <42994541+y-du@users.noreply.github.com>
2024-07-12 13:55:45 +0200

    remove state sync and simplify handler

--- handler/local_device_hdl/handler.go
+++ handler/local_device_hdl/handler.go
@@ -22,17 +22,16 @@ type device struct {
 }
 
 type Handler struct {
-	dmClient            dm_client.ClientItf
-	queryInterval       time.Duration
-	idPrefix            string
-	dChan               chan struct{}
-	devices             map[string]device
-	running             bool
-	loopMu              sync.RWMutex
-	ctx                 context.Context
-	cf                  context.CancelFunc
-	deviceSyncFunc      func(ctx context.Context, devices map[string]model.Device, newIDs, changedIDs, missingIDs []string) (recreated, createFailed, updateFailed, deleteFailed []string, err error)
-	deviceStateSyncFunc func(ctx context.Context, devices map[string]model.Device, isOnlineIDs, isOfflineIDs, isOnlineAgainIDs []string) (failed []string, err error)
+	dmClient       dm_client.ClientItf
+	queryInterval  time.Duration
+	idPrefix       string
+	dChan          chan struct{}
+	devices        map[string]device
+	running        bool
+	loopMu         sync.RWMutex
+	ctx            context.Context
+	cf             context.CancelFunc
+	deviceSyncFunc func(ctx context.Context, devices map[string]model.Device, newIDs, changedIDs, missingIDs, onlineIDs, offlineIDs []string)
 }
 
 func New(ctx context.Context, dmClient dm_client.ClientItf, queryInterval time.Duration, idPrefix string) *Handler {
@@ -62,12 +61,8 @@ func (h *Handler) Stop() {
 	<-h.dChan
 }
 
-func (h *Handler) SetDeviceSyncFunc(f func(ctx context.Context, devices map[string]model.Device, newIDs, changedIDs, missingIDs []string) (recreated, createFailed, updateFailed, deleteFailed []string, err error)) {
+func (h *Handler) SetDeviceSyncFunc(f func(ctx context.Context, devices map[string]model.Device, newIDs, changedIDs, missingIDs, onlineIDs, offlineIDs []string)) {
 	h.deviceSyncFunc = f
-}
-
-func (h *Handler) SetDeviceStateSyncFunc(f func(ctx context.Context, devices map[string]model.Device, isOnlineIDs, isOfflineIDs, isOnlineAgainIDs []string) (failed []string, err error)) {
-	h.deviceStateSyncFunc = f
 }
 
 func (h *Handler) run() {
@@ -80,8 +75,7 @@ func (h *Handler) run() {
 	for loop {
 		select {
 		case <-timer.C:
-			util.Logger.Debug(logPrefix, " begin devices refresh")
-			err = h.RefreshDevices(h.ctx)
+			err = h.refreshDevices(h.ctx)
 			if err != nil {
 				util.Logger.Errorf("%s %s", logPrefix, err)
 			}
@@ -103,129 +97,71 @@ func (h *Handler) run() {
 	h.dChan <- struct{}{}
 }
 
-func (h *Handler) RefreshDevices(ctx context.Context) error {
+func (h *Handler) refreshDevices(ctx context.Context) error {
+	util.Logger.Debug(logPrefix, " get devices")
 	ctxWc, cf := context.WithCancel(ctx)
 	defer cf()
 	dmDevices, err := h.dmClient.GetDevices(ctxWc)
 	if err != nil {
 		return fmt.Errorf("get devices: %s", err)
 	}
+	util.Logger.Debugf("%s got devices (%d)", logPrefix, len(dmDevices))
 	devices := make(map[string]device)
 	for id, dmDevice := range dmDevices {
 		devices[h.idPrefix+id] = newDevice(h.idPrefix+id, dmDevice)
-		util.Logger.Debugf("%s found device (%s, %s)", logPrefix, id, dmDevice.State)
 	}
-	var recreatedIDs []string
+	newIDs, changedIDs, missingIDs, onlineIDs, offlineIDs := h.diffDevices(devices)
+	if len(newIDs) > 0 {
+		util.Logger.Infof("%s new devices (%s)", logPrefix, strings.Join(newIDs, ", "))
+	}
+	if len(changedIDs) > 0 {
+		util.Logger.Infof("%s changed devices (%s)", logPrefix, strings.Join(changedIDs, ", "))
+	}
+	if len(missingIDs) > 0 {
+		util.Logger.Infof("%s missing devices (%s)", logPrefix, strings.Join(missingIDs, ", "))
+	}
+	if len(onlineIDs) > 0 {
+		util.Logger.Infof("%s online devices (%s)", logPrefix, strings.Join(onlineIDs, ", "))
+	}
+	if len(offlineIDs) > 0 {
+		util.Logger.Infof("%s offline devices (%s)", logPrefix, strings.Join(offlineIDs, ", "))
+	}
+	deviceMap := make(map[string]model.Device)
+	for id, dev := range devices {
+		deviceMap[id] = dev.Device
+	}
 	if h.deviceSyncFunc != nil {
-		newIDs, changedIDs, missingIDs, deviceMap := h.diffDevices(devices)
-		if len(newIDs) > 0 {
-			util.Logger.Infof("%s new devices (%s)", logPrefix, strings.Join(newIDs, ", "))
-		}
-		if len(changedIDs) > 0 {
-			util.Logger.Infof("%s changed devices (%s)", logPrefix, strings.Join(changedIDs, ", "))
-		}
-		if len(missingIDs) > 0 {
-			util.Logger.Infof("%s missing devices (%s)", logPrefix, strings.Join(missingIDs, ", "))
-		}
-		recreated, createFailed, updateFailed, deleteFailed, err := h.deviceSyncFunc(ctx, deviceMap, newIDs, changedIDs, missingIDs)
-		if err != nil {
-			return fmt.Errorf("sync devices: %s", err)
-		}
-		recreatedIDs = recreated
-		for _, id := range createFailed {
-			delete(devices, id)
-		}
-		for _, id := range updateFailed {
-			if d, ok := h.devices[id]; ok {
-				devices[id] = d
-			}
-		}
-		for _, id := range deleteFailed {
-			if d, ok := h.devices[id]; ok {
-				devices[id] = d
-			}
-		}
-	}
-	if h.deviceStateSyncFunc != nil {
-		isOnlineIDs, isOfflineIDs, isOnlineAgainIDs, deviceMap := h.diffDeviceStates(devices, recreatedIDs)
-		if len(isOnlineIDs) > 0 {
-			util.Logger.Infof("%s online devices (%s)", logPrefix, strings.Join(isOnlineIDs, ", "))
-		}
-		if len(isOfflineIDs) > 0 {
-			util.Logger.Infof("%s offline devices (%s)", logPrefix, strings.Join(isOfflineIDs, ", "))
-		}
-		failed, err := h.deviceStateSyncFunc(ctx, deviceMap, isOnlineIDs, isOfflineIDs, isOnlineAgainIDs)
-		if err != nil {
-			return fmt.Errorf("sync device states: %s", err)
-		}
-		for _, id := range failed {
-			d := devices[id]
-			od, ok := h.devices[id]
-			if ok {
-				if d.State != od.State {
-					d.State = od.State
-				} else {
-					d.State = model.Offline
-				}
-			} else {
-				d.State = ""
-			}
-			devices[id] = d
-		}
+		h.deviceSyncFunc(ctx, deviceMap, newIDs, changedIDs, missingIDs, onlineIDs, offlineIDs)
 	}
 	h.devices = devices
 	return nil
 }
 
-func (h *Handler) diffDevices(devices map[string]device) (newIDs, changedIDs, missingIDs []string, deviceMap map[string]model.Device) {
-	deviceMap = make(map[string]model.Device)
+func (h *Handler) diffDevices(devices map[string]device) (newIDs, changedIDs, missingIDs, onlineIDs, offlineIDs []string) {
 	for id, queriedDevice := range devices {
-		deviceMap[id] = queriedDevice.Device
 		storedDevice, ok := h.devices[id]
 		if ok {
 			if storedDevice.Hash != queriedDevice.Hash {
 				changedIDs = append(changedIDs, id)
 			}
+			if storedDevice.State != queriedDevice.State {
+				switch queriedDevice.State {
+				case model.Online:
+					onlineIDs = append(onlineIDs, id)
+				case model.Offline, "":
+					offlineIDs = append(offlineIDs, id)
+				}
+			}
 		} else {
 			newIDs = append(newIDs, id)
+			if queriedDevice.State == model.Online {
+				onlineIDs = append(onlineIDs, id)
+			}
 		}
 	}
 	for id := range h.devices {
 		if _, ok := devices[id]; !ok {
 			missingIDs = append(missingIDs, id)
-		}
-	}
-	return
-}
-
-func (h *Handler) diffDeviceStates(devices map[string]device, recreated []string) (isOnlineIDs, isOfflineIDs, isOnlineAgainIDs []string, deviceMap map[string]model.Device) {
-	deviceMap = make(map[string]model.Device)
-	for id, queriedDevice := range devices {
-		deviceMap[id] = queriedDevice.Device
-		storedDevice, ok := h.devices[id]
-		if ok {
-			if storedDevice.State != queriedDevice.State {
-				switch queriedDevice.State {
-				case model.Online:
-					isOnlineIDs = append(isOnlineIDs, id)
-				case model.Offline, "":
-					isOfflineIDs = append(isOfflineIDs, id)
-				}
-			}
-		} else {
-			if queriedDevice.State == model.Online {
-				isOnlineIDs = append(isOnlineIDs, id)
-			}
-		}
-	}
-	for id := range h.devices {
-		if _, ok := devices[id]; !ok {
-			isOfflineIDs = append(isOfflineIDs, id)
-		}
-	}
-	for _, id := range recreated {
-		if d, ok := devices[id]; ok && d.State == model.Online {
-			isOnlineAgainIDs = append(isOnlineAgainIDs, id)
 		}
 	}
 	return
