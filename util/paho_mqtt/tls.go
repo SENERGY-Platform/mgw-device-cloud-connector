bbd0cf8ce783a8e143699a9f34fbaae1dfd40097
y-du <42994541+y-du@users.noreply.github.com>
2024-09-25 13:32:03 +0200

    use utc time

--- util/paho_mqtt/tls.go
+++ util/paho_mqtt/tls.go
@@ -7,7 +7,7 @@ import (
 )
 
 func checkPeerCertValidityBounds(cs tls.ConnectionState) error {
-	currenTime := time.Now()
+	currenTime := time.Now().UTC()
 	for _, peerCert := range cs.PeerCertificates {
 		if currenTime.Before(peerCert.NotBefore) || currenTime.After(peerCert.NotAfter) {
 			return errors.New("certificate expired or not valid yet")
