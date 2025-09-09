26d69f8f6b3c420ff88347aa4dd4c9dfd409c166
y-du <42994541+y-du@users.noreply.github.com>
2025-09-09 14:28:09 +0200

    add test

--- handler/message_hdl/device_event_test.go
+++ handler/message_hdl/device_event_test.go
@@ -63,3 +63,43 @@ func TestHandleUpstreamDeviceEventAgeLimit(t *testing.T) {
 		}
 	})
 }
+
+func TestHandleUpstreamDeviceEvent(t *testing.T) {
+	t.Cleanup(clearVars)
+	topic.InitTopicHandler("usrID", "netID")
+	LocalDeviceIDPrefix = "123"
+	timestamp := time.Now()
+	mb, err := json.Marshal(CSEMetadata{Timestamp: timestamp.Format(time.RFC3339Nano)})
+	if err != nil {
+		t.Fatal(err)
+	}
+	a := CloudDeviceEventMsg{Metadata: string(mb), Data: "test"}
+	rt, rb, err := HandleUpstreamDeviceEvent(&mockMessage{
+		topic:     "event/a/b",
+		payload:   []byte("test"),
+		timestamp: timestamp,
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	if rt != topic.Handler.CloudDeviceEventPub(LocalDeviceIDPrefix+"a", "b") {
+		t.Error("expected", topic.Handler.CloudDeviceEventPub("a", "b"), "got", rt)
+	}
+	var b CloudDeviceEventMsg
+	err = json.Unmarshal(rb, &b)
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(a, b) {
+		t.Error("expected", a, "got", b)
+	}
+	t.Run("error", func(t *testing.T) {
+		_, _, err = HandleUpstreamDeviceEvent(&mockMessage{
+			topic:     "test",
+			timestamp: time.Now(),
+		})
+		if err == nil {
+			t.Error("expected error")
+		}
+	})
+}
